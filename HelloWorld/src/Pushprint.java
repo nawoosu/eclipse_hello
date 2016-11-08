5a9d85c74089f072d8936e6133c053f3b6d33359
nawoosu <pepero600@naver.com>
2016-11-08 15:29:22 +0900

    Update Pushprint.java

--- HelloWorld/src/Pushprint.java
+++ HelloWorld/src/Pushprint.java
@@ -9,7 +9,7 @@ public class Pushprint {
 		System.out.println("local master");
 		System.out.println("World with 365");
 		System.out.println("remote master");
-		System.out.println("World with GitHub")
+		System.out.println("World with GitHub");
 	}
 
 }
